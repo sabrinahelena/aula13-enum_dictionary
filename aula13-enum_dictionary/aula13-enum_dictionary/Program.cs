c5bdad443c764dce4f58643b03e8a19a3e01b065
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-06-21 16:01:24 -0300

    adicionei projeto

---
+++ aula13-enum_dictionary/aula13-enum_dictionary/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace aula13_enum_dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
